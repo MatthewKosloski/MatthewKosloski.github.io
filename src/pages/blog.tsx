be3288e66a7eae635d9d1fc84fc2e57953a09ec6
Matthew Kosloski <matthewkosloski@me.com>
2022-01-16 00:24:51 -0600

    initial blog page

---
+++ src/pages/blog.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { WindowLocation } from '@reach/router';
+import {
+  Grid,
+  GridCol,
+  Layout,
+  Section,
+} from '../components';
+
+interface Props {
+  location: WindowLocation;
+}
+
+function BlogPage({ location }: Props) {
+  return (
+    <Layout
+      location={location}
+      pageTitle="Blog"
+    >
+      <Section>
+        <Grid>
+          <GridCol xs={12} md={10} mdOffset={2} lg={8} lgOffset={3}>
+            <p>Blog posts go here :)</p>
+          </GridCol>
+        </Grid>
+      </Section>
+    </Layout>
+  );
+}
+
+export default BlogPage;
