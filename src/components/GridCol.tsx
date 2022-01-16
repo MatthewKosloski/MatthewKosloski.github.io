2948c67c36ad93e76700ebe3b03943a562637a45
Matthew Kosloski <matthewkosloski@me.com>
2022-01-16 00:18:45 -0600

    added alignCenter variants to GridCol

--- src/components/GridCol.tsx
+++ src/components/GridCol.tsx
@@ -12,7 +12,18 @@ interface GridColProps {
   lgOffset?: number;
   xl?: number;
   xlOffset?: number;
+  alignCenterXs?: boolean;
+  alignCenterSm?: boolean;
+  alignCenterMd?: boolean;
+  alignCenterLg?: boolean;
+  alignCenterXl?: boolean;
 }
+
+const alignCenterCss = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const GridCol = styled.div<GridColProps>`
   ${({ xs, xsOffset }) =>
@@ -59,6 +70,21 @@ const GridCol = styled.div<GridColProps>`
           ${({ xl }) => (xl ? `grid-column-end: span ${xl}` : '')};
         `)
       : ''}
+
+  ${({ alignCenterXs }) =>
+    alignCenterXs && breakpoint(Breakpoints.XS)(alignCenterCss)};
+
+  ${({ alignCenterSm }) =>
+    alignCenterSm && breakpoint(Breakpoints.SM)(alignCenterCss)};
+
+  ${({ alignCenterMd }) =>
+    alignCenterMd && breakpoint(Breakpoints.MD)(alignCenterCss)};
+
+  ${({ alignCenterLg }) =>
+    alignCenterLg && breakpoint(Breakpoints.LG)(alignCenterCss)};
+
+  ${({ alignCenterXl }) =>
+    alignCenterXl && breakpoint(Breakpoints.XL)(alignCenterCss)};
 `;
 
 export default GridCol;
