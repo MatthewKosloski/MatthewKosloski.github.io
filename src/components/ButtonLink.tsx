5d33f6313de4e89ccda0a2f803e6f2270b348f50
Matthew Kosloski <matthewkosloski@me.com>
2022-01-22 23:12:30 -0600

    change button display css

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -50,7 +50,7 @@ const Wrapper = styled.a<WrapperProps>`
 	text-decoration: none;
 	transition: background-color 0.25s ease-in-out, color 0.25s ease-in-out;
 	text-align: center;
-	display: block;
+	display: inline-block;
 	text-transform: uppercase;
 	letter-spacing: ${(p) => p.theme.typography.letterSpacingEm};
 	font-weight: 700;
