1ebcb5bb6cd47c28e81e825127909aa929fef610
Matthew Kosloski <matthewkosloski@me.com>
2022-01-23 13:23:09 -0600

    updated 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
+import { Link as GatsbyLink } from 'gatsby';
+import { Grid, GridCol, Layout, Section } from '../components';
+import { BasePageProps } from './types';
 
-const NotFoundPage = () => {
+function NotFoundPage({ location }: BasePageProps) {
 	return (
-		<main>
-			<h1>404 Not Found</h1>
-		</main>
+		<Layout
+			location={location}
+			pageTitle="Uh Oh..."
+			pageSubtitle="404 Not Found"
+		>
+			<Section>
+				<Grid>
+					<GridCol xs={12} md={10} mdOffset={2} lg={8} lgOffset={3}>
+						<p className="lead">
+							The page you are looking for doesn't exist or has been moved.
+						</p>
+						<p>
+							You can return to the{' '}
+							<GatsbyLink to="/">overview page</GatsbyLink> or email me at{' '}
+							<em>matthew at mtk dot me</em> if you can't find what you're
+							looking for.
+						</p>
+					</GridCol>
+				</Grid>
+			</Section>
+		</Layout>
 	);
-};
+}
 
 export default NotFoundPage;
