b6b3e1ab25f97da9590adfccf1d9b84068c82ebf
Matthew Kosloski <matthewkosloski@me.com>
2022-01-23 20:15:47 -0600

    changes to experience page

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -12,6 +12,7 @@ import {
 	SubtleLinkExternal,
 } from '../components';
 import { ButtonLinkVariant } from '../components/ButtonLink';
+import { StaticImage } from 'gatsby-plugin-image';
 import { BasePageProps } from './types';
 
 function ExperiencePage({ location }: BasePageProps) {
@@ -36,28 +37,46 @@ function ExperiencePage({ location }: BasePageProps) {
 			</Section>
 			<Section title="Education">
 				<Grid>
-					<GridCol xs={12} md={10} mdOffset={2} lg={8} lgOffset={3}>
-						<SubSectionHeader>
+					<GridCol xs={12}>
+						<SubSectionHeader noBottomMargin>
 							<h3>Purdue University Northwest</h3>
 							<h4>
 								Aug. '18 <span>&mdash;</span> May '21
 							</h4>
 						</SubSectionHeader>
+					</GridCol>
+					<GridCol xs={12} md={6} orderLastXs orderInitialMd>
 						<p>
-							Bachelor of Science, Computer Science; Graduated with distinction
+							B.S. in Computer Science; Graduated with distinction
 							(top 10%).
 						</p>
 						<p>
 							For my senior project, I designed a high-level, Lisp-like
 							programming language and implemented it by building a compiler
 							from scratch. The compiler translates the high-level program to
-							x86-64 assembly code (AT&amp;T syntax).
+							x86-64 assembly code in AT&amp;T syntax.
 						</p>
 						<Flex>
 							<FlexCol>
 								<SubtleLinkExternal href="#">
 									Read my senior paper
 								</SubtleLinkExternal>
+							</FlexCol>
+						</Flex>
+					</GridCol>
+					<GridCol xs={12} md={6}>
+						<Flex justifyCenterXs justifyNormalMd>
+							<FlexCol>
+								<StaticImage
+									src="../images/photos/matthew-kosloski-graduation.jpg"
+									alt="A portrait photo of Matthew Kosloski wearing graduation attire and holding his diploma"
+									placeholder="blurred"
+									loading="lazy"
+									imgClassName="rounded"
+									width={430}
+									height={430}
+									quality={100}
+								/>
 							</FlexCol>
 						</Flex>
 					</GridCol>
