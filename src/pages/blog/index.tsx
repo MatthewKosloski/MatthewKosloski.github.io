e4a1648464fceee310b27fedddf49d70a5076e76
Matthew Kosloski <matthewkosloski@me.com>
2022-01-24 00:25:27 -0600

    created initial blog index and post page

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import { Grid, GridCol, Layout, Section } from '../../components';
+import { BasePageProps } from '../types';
+
+function BlogPage({ location, data }: BasePageProps & BlogQuery) {
+	return (
+		<Layout location={location} pageTitle="Blog">
+			<Section>
+				<Grid>
+					<GridCol xs={12} md={10} mdOffset={2} lg={8} lgOffset={3}>
+					<ul>
+					{
+						data.allMdx.nodes.map(({ id, slug, frontmatter }) => (
+							<li key={id}>
+								<a href={slug}>
+									<h3>{frontmatter.title}</h3>
+									<p>{frontmatter.date}</p>
+								</a>
+							</li>
+						))
+					}
+					</ul>
+					</GridCol>
+				</Grid>
+			</Section>
+		</Layout>
+	);
+}
+
+interface BlogQuery {
+	data: {
+		allMdx: {
+			nodes: {
+				frontmatter: {
+					title: string;
+					date: string;
+				}
+				id: string;
+				slug: string;
+			}[]
+		}
+	}
+}
+
+export const query = graphql`
+{
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+      id
+      slug
+    }
+  }
+}
+`;
+
+export default BlogPage;
---
+++ src/pages/blog/{mdx.slug}.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { Grid, GridCol, Layout, Section } from '../../components';
+import { BasePageProps } from '../types';
+
+function BlogPost({ location, data }: BasePageProps & BlogPostQuery) {
+	return (
+		<Layout 
+			location={location}
+			pageTitle={data.mdx.frontmatter.title}
+			pageSubtitle={data.mdx.frontmatter.date}>
+			<Section>
+				<Grid>
+					<GridCol xs={12} md={10} mdOffset={2} lg={8} lgOffset={3}>
+						<MDXRenderer>
+							{data.mdx.body}
+						</MDXRenderer>
+					</GridCol>
+				</Grid>
+			</Section>
+		</Layout>
+	);
+}
+
+interface BlogPostQuery {
+	data: {
+		mdx: {
+			frontmatter: {
+				title: string;
+				date: string;
+			}
+			body: string;
+		}
+	}
+}
+
+export const query = graphql`
+	query ($id: String) {
+		mdx(id: {eq: $id}) {
+			frontmatter {
+				title
+				date(formatString: "MMMM D, YYYY")
+			}
+			body
+		}
+	}
+`;
+
+export default BlogPost;
