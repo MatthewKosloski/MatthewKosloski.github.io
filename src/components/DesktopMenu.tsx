5ffa74cae388ff2501c5a4ace646ee6182b594c6
Matthew Kosloski <matthewkosloski@me.com>
2022-02-27 21:41:39 -0600

    refactored DesktopMenu

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link as GatsbyLink } from 'gatsby';
 import { MenuLink } from '../hooks/useMenuLinks';
 
 const borderThickness = 4;
 
 const Wrapper = styled.ul`
-	list-style: none;
-	display: none;
-	margin-bottom: 0;
-	${({ theme }) => `${theme.media.sm} {
-    display: flex;
-  }`}
+	${({ theme: { media } }) => css`
+		${media.sm} {
+			display: flex;
+		}
+		list-style: none;
+		display: none;
+		margin-bottom: 0;
+	`}
 `;
 
 const MenuItem = styled.li`
@@ -19,39 +21,40 @@ const MenuItem = styled.li`
 `;
 
 const MenuItemLink = styled(GatsbyLink)<{ $isActive: boolean }>`
-	display: inline-block;
-	font-size: ${(p) => p.theme.utils.pxToRem(13)};
-	line-height: 1;
-	text-decoration: none;
-	font-weight: 700;
-	text-transform: uppercase;
-	color: ${(p) =>
-		p.$isActive ? p.theme.color.eastSidePurple : p.theme.color.white500};
-	letter-spacing: ${(p) => p.theme.utils.pxToEm(1)};
-	transition: color 0.15s ease-in-out;
-	padding: calc(
-			${(p) => p.theme.vr.two.rem} -
-				${(p) => p.theme.utils.pxToEm(borderThickness)}
-		)
-		${(p) => p.theme.vr.quarter.rem} ${(p) => p.theme.vr.half.rem}
-		${(p) => p.theme.vr.quarter.rem};
-	&:before {
-		content: '';
-		display: block;
-		width: calc(100% - (${(p) => p.theme.vr.quarter.rem} * 2));
-		height: ${(p) => p.theme.utils.pxToEm(borderThickness)};
-		background-color: ${(p) =>
-			p.$isActive ? p.theme.color.eastSidePurple : 'transparent'};
-		top: 0;
-		left: ${(p) => p.theme.vr.quarter.rem};
-		position: absolute;
-	}
-	&:hover {
-		color: ${(p) => p.theme.color.eastSidePurple};
-	}
+	${({ theme: { color, vr, utils }, $isActive }) => css`
+		display: inline-block;
+		font-size: ${utils.pxToRem(13)};
+		line-height: 1;
+		text-decoration: none;
+		font-weight: 700;
+		text-transform: uppercase;
+		color: ${$isActive ? color.eastSidePurple : color.white500};
+		letter-spacing: ${utils.pxToEm(1)};
+		transition: color 0.15s ease-in-out;
+		padding: calc(${vr.two.rem} - ${utils.pxToEm(borderThickness)})
+			${vr.quarter.rem} ${vr.half.rem} ${vr.quarter.rem};
+		&:before {
+			content: '';
+			display: block;
+			width: calc(100% - (${vr.quarter.rem} * 2));
+			height: ${utils.pxToEm(borderThickness)};
+			background-color: ${$isActive ? color.eastSidePurple : 'transparent'};
+			top: 0;
+			left: ${vr.quarter.rem};
+			position: absolute;
+		}
+		&:hover {
+			color: ${color.eastSidePurple};
+		}
+	`}
 `;
 
-function renderMenuItems(pathname: string, menuLinks: MenuLink[]) {
+interface DesktopMenuProps {
+	pathname: string;
+	menuLinks: MenuLink[];
+}
+
+function renderMenuItems({ pathname, menuLinks }: DesktopMenuProps) {
 	return menuLinks.map(
 		({ text, path, id }: { text: string; path: string; id: string }) => (
 			<MenuItem key={id}>
@@ -63,13 +66,8 @@ function renderMenuItems(pathname: string, menuLinks: MenuLink[]) {
 	);
 }
 
-interface DesktopMenuProps {
-	pathname: string;
-	menuLinks: MenuLink[];
-}
-
-function DesktopMenu({ pathname, menuLinks }: DesktopMenuProps) {
-	return <Wrapper>{renderMenuItems(pathname, menuLinks)}</Wrapper>;
+function DesktopMenu(props: DesktopMenuProps) {
+	return <Wrapper>{renderMenuItems(props)}</Wrapper>;
 }
 
 export default DesktopMenu;
